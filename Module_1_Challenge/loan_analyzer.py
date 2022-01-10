199e3205ca90b2b65f10a6da42d0f7cb1e99c2ed
Tom Cloud <cloud.thomas@gmail.com>
2022-01-09 19:43:29 -0600

    finished?

--- Module_1_Challenge/loan_analyzer.py
+++ Module_1_Challenge/loan_analyzer.py
@@ -3,9 +3,7 @@ import csv
 from pathlib import Path
 
 """Part 1: Automate the Calculations.
-
 Automate the calculations for the loan portfolio summaries.
-
 First, let's start with some calculations on a list of prices for 5 loans.
     1. Use the `len` function to calculate the total number of loans in the list.
     2. Use the `sum` function to calculate the total of all loans in the list.
@@ -38,24 +36,18 @@ print("The avergae loan amount is $",avg_loan_amount)
 
 
 """Part 2: Analyze Loan Data.
-
 Analyze the loan to determine the investment evaluation.
-
 Using more detailed data on one of these loans, follow these steps to calculate a Present Value, or a "fair price" for what this loan would be worth.
-
 1. Use get() on the dictionary of additional information to extract the **Future Value** and **Remaining Months** on the loan.
     a. Save these values as variables called `future_value` and `remaining_months`.
     b. Print each variable.
-
     @NOTE:
     **Future Value**: The amount of money the borrower has to pay back upon maturity of the loan (a.k.a. "Face Value")
     **Remaining Months**: The remaining maturity (in months) before the loan needs to be fully repaid.
-
 2. Use the formula for Present Value to calculate a "fair value" of the loan. Use a minimum required return of 20% as the discount rate.
 3. Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
     a. If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
     b. Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-
     @NOTE:
     If Present Value represents the loan's fair value (given the required minimum return of 20%), does it make sense to buy the loan at its current cost?
 """
@@ -102,9 +94,7 @@ print(f"The present value of the loan is: {present_value: .2f}")
 
 
 """Part 3: Perform Financial Calculations.
-
 Perform financial calculations using functions.
-
 1. Define a new function that will be used to calculate present value.
     a. This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
     b. The function should return the `present_value` for the loan.
@@ -144,9 +134,7 @@ present_value_finder(12,1000,.20)
 
 
 """Part 4: Conditionally filter lists of loans.
-
 In this section, you will use a loop to iterate through a series of loans and select only the inexpensive loans.
-
 1. Create a new, empty list called `inexpensive_loans`.
 2. Use a for loop to select each loan from a list of loans.
     a. Inside the for loop, write an if-statement to determine if the loan_price is less than or equal to 500
@@ -194,9 +182,6 @@ for item in loans:
     loan_price=item["loan_price"]
     if loan_price <=500:
         inexspensive_loans.append(loan_price)
-# print("The inexspensive loans are:")
-
-
 
 
 # @TODO: Print the `inexpensive_loans` list
@@ -205,17 +190,14 @@ for item in loans:
 print (inexspensive_loans)
 
 """Part 5: Save the results.
-
 Output this list of inexpensive loans to a csv file
     1. Use `with open` to open a new CSV file.
         a. Create a `csvwriter` using the `csv` library.
         b. Use the new csvwriter to write the header variable as the first row.
         c. Use a for loop to iterate through each loan in `inexpensive_loans`.
             i. Use the csvwriter to write the `loan.values()` to a row in the CSV file.
-
     Hint: Refer to the official documentation for the csv library.
     https://docs.python.org/3/library/csv.html#writer-objects
-
 """
 
 # Set the output header
@@ -227,3 +209,15 @@ output_path = Path("inexpensive_loans.csv")
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
 # YOUR CODE HERE!
+
+import csv
+from pathlib import Path
+
+csvpath = Path("inexspensive_loans.csv")
+print("Writing the data to a csv file...")
+
+with open(csvpath, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=",")
+    csvwriter.writerow(header)
+    for item in inexspensive_loans:
+        csvwriter.writerow(loan.values())
